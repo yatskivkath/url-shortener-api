9ae13f4b5f4257df3a4c547a39e72123a3bc58a2
“Kateryna <yatskivkath@gmail.com>
2024-04-04 21:36:25 +0200

    Add bcrypt dependency and create user seeder

---
+++ src/db/seeders/20240404162442-demo-user.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface) {
+        const hashedPassword = await bcrypt.hash('root', 10);
+
+        await queryInterface.bulkInsert('users', [
+            {
+                first_name: 'Admin',
+                last_name: 'Admin',
+                email: 'root@mail.com',
+                password: hashedPassword,
+                id: crypto.randomUUID(),
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+        ]);
+    },
+
+    async down(queryInterface) {
+        await queryInterface.bulkDelete('users', null, {});
+    },
+};
