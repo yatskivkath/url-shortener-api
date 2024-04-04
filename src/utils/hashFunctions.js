24a12b180b80790fb1d67d6dbadcad8ca3811d0c
“Kateryna <yatskivkath@gmail.com>
2024-04-04 22:45:51 +0200

    Add hash generation function

---
+++ src/utils/hashFunctions.js
@@ -0,0 +1,23 @@
+// hashFunctions.js
+// Implementation of the hash generation function
+
+const CHARACTERS =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const DEFAULT_LENGTH = 5;
+
+const generateHash = (len = DEFAULT_LENGTH) => {
+    if (!Number(len)) {
+        throw new Error('Please send a correct string length');
+    }
+
+    let result = '';
+    for (let i = 0; i < len; i++) {
+        result += CHARACTERS.charAt(
+            Math.floor(Math.random() * CHARACTERS.length)
+        );
+    }
+
+    return result;
+};
+
+export { generateHash };
