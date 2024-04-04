a8841902b192e31e46e50e4ac5056a9f09aa3cdd
“Kateryna <yatskivkath@gmail.com>
2024-04-04 22:54:31 +0200

    Add urlService.js implementation

---
+++ src/services/urlService.js
@@ -0,0 +1,60 @@
+// urlService.js
+// Implementation of the Url service
+
+const urlRepository = require('../repositories/urlRepository.js');
+const { generateHash } = require('../utils/hashFunctions.js');
+const scopes = require('../constants/scopes.js');
+
+async function createUrl(url) {
+    const { redirectUrl, userId } = url;
+
+    const newUrl = await urlRepository.saveUrl({
+        code: generateHash(),
+        url: redirectUrl,
+        user_id: userId,
+    });
+
+    return newUrl;
+}
+
+async function getUrl(code) {
+    const url = await urlRepository.findUrlByCode(code);
+
+    return url;
+}
+
+async function getUrlPublic(code) {
+    const url = await urlRepository.findUrlByCode(code, scopes.url.public);
+
+    return url;
+}
+
+async function getUrlsByUserPublic(userId) {
+    const urls = await urlRepository.getAllUrlsByUserId(
+        userId,
+        scopes.url.public
+    );
+
+    return urls;
+}
+
+async function visitUrl(code) {
+    const url = await urlRepository.findUrlByCode(code);
+
+    if (!url) {
+        throw new Error('Url was not found');
+    }
+
+    url.visits += 1;
+    await urlRepository.updateUrl(url);
+
+    return url;
+}
+
+module.exports = {
+    createUrl,
+    getUrl,
+    getUrlPublic,
+    getUrlsByUserPublic,
+    visitUrl,
+};
