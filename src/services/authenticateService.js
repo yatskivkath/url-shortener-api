8aa93ec56e2a52c6a6ca826356df234608eb34c1
“Kateryna <yatskivkath@gmail.com>
2024-04-04 22:36:15 +0200

    Add authenticateService and userService modules

---
+++ src/services/authenticateService.js
@@ -0,0 +1,21 @@
+// authenticateService.js
+
+const bcrypt = require('bcrypt');
+
+async function hashPassword(password) {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    return hashedPassword;
+}
+
+async function comparePasswords(password, hashedPassword) {
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+
+    return isMatch;
+}
+
+module.exports = {
+    hashPassword,
+    comparePasswords,
+};
---
+++ src/services/userService.js
@@ -0,0 +1,57 @@
+// userService.js
+
+const userRepository = require('../repositories/userRepository.js');
+const authenticateService = require('./authenticateService.js');
+const scopes = require('../constants/scopes.js');
+
+async function createUser(user) {
+    const { firstName, lastName, email, password } = user;
+    const hashedPassword = await authenticateService.hashPassword(password);
+
+    const newUser = await userRepository.saveUser({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+    });
+
+    return newUser;
+}
+
+async function getUserByEmail(email) {
+    const user = await userRepository.findUserByEmail(email);
+
+    if (!user) {
+        throw new Error('User not found');
+    }
+
+    return user;
+}
+
+async function getUsersPublic() {
+    const users = await userRepository.getAllUsers(scopes.user.public);
+
+    return users;
+}
+
+async function checkPassword(email, password) {
+    const user = await getUserByEmail(email);
+
+    if (!user) {
+        return false;
+    }
+
+    const isMatch = await authenticateService.comparePasswords(
+        password,
+        user.password
+    );
+
+    return isMatch;
+}
+
+module.exports = {
+    createUser,
+    getUserByEmail,
+    getUsersPublic,
+    checkPassword,
+};
