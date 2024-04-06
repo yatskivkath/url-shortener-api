0b41a3f597406704f616a86c63de5f6b06854d54
“Kateryna <yatskivkath@gmail.com>
2024-04-06 13:51:24 +0200

    Add Swagger documentation for URL endpoints

--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -9,10 +9,94 @@ const urlRouter = express.Router();
 
 urlRouter.use(authMiddleware);
 
+/**
+ * @swagger
+ * /urls:
+ *  post:
+ *   tags:
+ *   - Url
+ *   summary: Create a new short URL
+ *   requestBody:
+ *     required: true
+ *     content:
+ *       application/json:
+ *         schema:
+ *           type: object
+ *           required:
+ *             - url
+ *             - name
+ *           properties:
+ *             url:
+ *               type: string
+ *               example: https://www.google.com
+ *             name:
+ *               type: string
+ *               example: Google
+ *             type:
+ *               type: string
+ *               enum: [P, T, OT]
+ *               example: P
+ *             code:
+ *               type: string
+ *               example: google-short
+ *             expire:
+ *               type: string
+ *               format: date
+ *               example: 2022-12-31
+ *   responses:
+ *      201:
+ *        description: Created
+ *      409:
+ *        description: Conflict
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
 urlRouter.post('/', urlController.createUrl);
 
+/**
+ * @swagger
+ * '/urls/info/{code}':
+ *  get:
+ *     tags:
+ *     - Url
+ *     summary: Get URL info
+ *     parameters:
+ *       - name: code
+ *         in: path
+ *         description: The shorted URL code
+ *         example: google-short
+ *         required: true
+ *     responses:
+ *      200:
+ *        description: Fetched Successfully
+ *      400:
+ *        description: Bad Request
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
 urlRouter.get('/info/:code', urlController.getUrl);
 
+/**
+ * @swagger
+ * '/urls':
+ *  get:
+ *     tags:
+ *     - Url
+ *     summary: Get all URLs for the currrent user
+ *     responses:
+ *      200:
+ *        description: Fetched Successfully
+ *      400:
+ *        description: Bad Request
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
 urlRouter.get('/', urlController.getAllUrlsByUserId);
 
 module.exports = urlRouter;
