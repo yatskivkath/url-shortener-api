0f8ebd0f58abe46c99844fa7f0264483f5685f48
“Kateryna <yatskivkath@gmail.com>
2024-04-06 13:31:57 +0200

    Docummented user endpoints

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -7,10 +7,68 @@ const authMiddleware = require('../middlewares/authMiddleware.js');
 
 const userRouter = express.Router();
 
+/**
+ * @swagger
+ * '/users':
+ *  post:
+ *     tags:
+ *     - User
+ *     summary: Create a user
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - firstName
+ *              - lastName
+ *              - email
+ *              - password
+ *            properties:
+ *              firstName:
+ *                type: string
+ *                example: John
+ *              lastName:
+ *                type: string
+ *                example: Doe
+ *              email:
+ *                type: string
+ *                example: joedoe20@mail.com
+ *              password:
+ *                type: string
+ *                example: johnDoe20!@
+ *     responses:
+ *      201:
+ *        description: Created
+ *      409:
+ *        description: Conflict
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
 userRouter.post('/', userController.createUser);
 
 userRouter.use(authMiddleware);
 
+/**
+ * @swagger
+ * '/users':
+ *  get:
+ *     tags:
+ *     - User
+ *     summary: Get all users
+ *     responses:
+ *      200:
+ *        description: Fetched Successfully
+ *      400:
+ *        description: Bad Request
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
 userRouter.get('/', userController.getAllUsers);
 
 module.exports = userRouter;
