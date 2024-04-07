055d4c4596da55aa5fdf7df56df57576707590f5
“Kateryna <yatskivkath@gmail.com>
2024-04-07 12:18:19 +0200

    Add swagger documentation for URL redirection

--- src/routes/codeRouter.js
+++ src/routes/codeRouter.js
@@ -7,6 +7,27 @@ const { redirectByCode } = require('../controllers/codeController.js');
 
 const codeRouter = new express.Router();
 
+/**
+ * @swagger
+ * '/rediredt/{code}':
+ *  get:
+ *     tags:
+ *     - Code
+ *     summary: Redirect to the original URL
+ *     parameters:
+ *       - name: code
+ *         in: path
+ *         description: The shorted URL code
+ *         example: google-short
+ *         required: true
+ *     responses:
+ *      302:
+ *        description: Redirect
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
 codeRouter.get('/:code', redirectByCode);
 
 module.exports = codeRouter;
