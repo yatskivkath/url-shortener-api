f3241575c6e2263c03f6788669500b48e137d547
“Kateryna <yatskivkath@gmail.com>
2024-04-13 14:01:27 +0200

    Add admin and home page templates

--- src/routes/pagesRouter.js
+++ src/routes/pagesRouter.js
@@ -2,15 +2,19 @@
 // Implementation of the ejs pages router
 
 const express = require('express');
+
 const pagesController = require('../controllers/pagesController.js');
+const authMiddleware = require('../middlewares/authMiddleware.js');
 
 const pagesRouter = express.Router();
-
-pagesRouter.get('/', pagesController.homePage);
 
 pagesRouter.get('/login', pagesController.loginPage);
 
 pagesRouter.get('/register', pagesController.registerPage);
+
+pagesRouter.use(authMiddleware);
+
+pagesRouter.get('/', pagesController.homePage);
 
 pagesRouter.get('/dasboard', pagesController.dashboardPage);
 
