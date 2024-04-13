e78cc44331ef90445215932e415bb71af302031d
“Kateryna <yatskivkath@gmail.com>
2024-04-13 17:19:46 +0200

    Remove console.log

--- src/permissions/userPermissions.js
+++ src/permissions/userPermissions.js
@@ -4,7 +4,6 @@
 const { actions, subjects } = require('../constants/permissionsConstants');
 
 module.exports = (user, { can, cannot }) => {
-    console.log('userPermissions.js', user);
     can(actions.MANAGE, subjects.URL, { user_id: user.id });
     can(actions.READ, subjects.URL);
 };
