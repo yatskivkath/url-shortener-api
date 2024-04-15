4a62dd08f8e22d1813de379ed042b5c622b5e17b
“Kateryna <yatskivkath@gmail.com>
2024-04-15 23:46:01 +0200

    Rewrite getUrlCodePublic.test.js with sequelize-mock

--- test/services/urlService/getUrlByCodePublic.test.js
+++ test/services/urlService/getUrlByCodePublic.test.js
@@ -1,64 +1,98 @@
 const { NotFound } = require('../../../src/errors/errors.js');
 
-jest.mock('../../../src/repositories/urlRepository.js', () => {
-    const urls = [
-        {
-            typeParsed: 'Permanent',
-            active: true,
-            shortUrl: 'http://localhost:3001/redirect/tlwnd',
-            userId: 'c9bb5609-4588-4d7d-bb18-c3e430ac7377',
-            //userId: '3b365a11-97a3-4d44-8137-b944cade14da',
-            id: '701db3f5-59f1-443c-beeb-50ff31c5b14f',
-            url: 'https://tailwindcss.com/',
-            name: 'Tailwind',
-            code: 'tlwnd',
-            visits: 2,
-            type: 'P',
-            enabled: false,
-            expirationdDate: null,
-        },
-    ];
+jest.mock('../../../src/models/user.js', () => {
+    const SequelizeMock = require('sequelize-mock');
+    const dbMock = new SequelizeMock();
 
-    return {
-        findUrlByCode: jest.fn().mockImplementation((code) => {
-            return urls.find((url) => url.code === code) ?? null;
-        }),
-    };
+    return dbMock.define('user', {
+        id: '3b365a11-97a3-4d44-8137-b944cade14da',
+        first_name: 'Kateyna',
+        last_name: 'Yatskiv',
+        email: 'kath@mail.com',
+        password:
+            '$2b$10$M4MkWr595pnNyfA.vS6OSOOMeMAfUTGjnzMf.zg5QIAA9rCkkJBdi',
+        createdAt: '2024-04-06T10:33:13.050Z',
+        updatedAt: '2024-04-06T10:33:13.050Z',
+        role: 'user',
+    });
 });
 
-jest.mock('../../../src/repositories/userRepository.js', () => {
-    return {};
+jest.mock('../../../src/models/url.js', () => {
+    const SequelizeMock = require('sequelize-mock');
+    const dbMock = new SequelizeMock();
+
+    return dbMock.define('url', {
+        typeParsed: 'Permanent',
+        active: true,
+        shortUrl: 'http://localhost:3001/redirect/knzqR',
+        id: '4145686d-4ec0-44e7-a711-36fbf3dd3af6',
+        code: 'knzqR',
+        url: 'https://google.com',
+        name: 'test',
+        visits: 0,
+        userId: '3b365a11-97a3-4d44-8137-b944cade14da',
+        expirationdDate: null,
+        type: 'P',
+        enabled: true,
+        createdAt: '2024-04-14T17:44:27.951Z',
+        updatedAt: '2024-04-14T17:44:27.951Z',
+    });
+});
+
+jest.mock('../../../src/models/index.js', () => {
+    const SequelizeMock = require('sequelize-mock');
+    const dbMock = new SequelizeMock();
+
+    const url = require('../../../src/models/url.js');
+    const user = require('../../../src/models/user.js');
+
+    return {
+        url,
+        user,
+        sequelize: dbMock,
+        Sequelize: SequelizeMock,
+    };
 });
 
 describe('Url Service getUrlByCodePublic function', () => {
     const urlService = require('../../../src/services/urlService.js');
+
+    const url = require('../../../src/models/url.js');
+    const user = require('../../../src/models/user.js');
 
     beforeEach(() => {
         jest.resetModules();
     });
 
     it('should return the url by code', async () => {
-        const CODE = 'tlwnd';
-        const url = await urlService.getUrlByCodePublic(CODE);
+        const CODE = 'knzqR';
 
-        expect(url).toEqual({
+        url.$queueResult(url.build({ code: CODE }));
+
+        const result = await urlService.getUrlByCodePublic(CODE);
+
+        expect(result).toEqual({
             typeParsed: 'Permanent',
             active: true,
-            shortUrl: 'http://localhost:3001/redirect/tlwnd',
-            userId: 'c9bb5609-4588-4d7d-bb18-c3e430ac7377',
-            id: '701db3f5-59f1-443c-beeb-50ff31c5b14f',
-            url: 'https://tailwindcss.com/',
-            name: 'Tailwind',
-            code: 'tlwnd',
-            visits: 2,
-            type: 'P',
-            enabled: false,
+            shortUrl: 'http://localhost:3001/redirect/knzqR',
+            id: '4145686d-4ec0-44e7-a711-36fbf3dd3af6',
+            code: 'knzqR',
+            url: 'https://google.com',
+            name: 'test',
+            visits: 0,
+            userId: '3b365a11-97a3-4d44-8137-b944cade14da',
             expirationdDate: null,
+            type: 'P',
+            enabled: true,
+            createdAt: '2024-04-14T17:44:27.951Z',
+            updatedAt: '2024-04-14T17:44:27.951Z',
         });
     });
 
     it('should throw an error if url is not found', async () => {
         const CODE = 'dummy';
+
+        url.$queueResult(null);
 
         await expect(urlService.getUrlByCodePublic(CODE)).rejects.toThrow(
             NotFound
@@ -69,6 +103,8 @@ describe('Url Service getUrlByCodePublic function', () => {
         const INVALID_CODES = [null, undefined, 0, false, NaN, ''];
 
         INVALID_CODES.forEach(async (code) => {
+            url.$queueResult(null);
+
             await expect(urlService.getUrlByCodePublic(code)).rejects.toThrow(
                 NotFound
             );
